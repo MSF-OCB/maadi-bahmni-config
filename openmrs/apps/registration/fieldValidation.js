5d01c7d28435406896f8083dcf6be3f46c502aa1
matirp235 <dpritam@thoughtworks.com>
2018-08-22 21:12:27 +0530

    Pritam | Fixing Feedback changes of Registartion page

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -5,16 +5,10 @@ Bahmni.Registration.customValidator = {
         },
         errorMessage: "REGISTRATION_AGE_ERROR_KEY"
     },
-    "Telephone Number": {
-        method: function (name, value, personAttributeDetails) {
-            return value && value.length> 6;
+    "In-scope Date": {
+        method:function (name,value,personAttributeDetails) {
+            return value <= new Date();
         },
-        errorMessage: "REGISTRATION_TELEPHONE_NUMBER_ERROR_KEY"
-    },
-    "caste": {
-        method: function (name, value, personAttributeDetails) {
-            return value.match(/^\w+$/);
-        },
-        errorMessage: "REGISTRATION_CASTE_TEXT_ERROR_KEY"
+        errorMessage: "REGISTRATION_IN_SCOPE_DATE_ERROR_KEY"
     }
 };
