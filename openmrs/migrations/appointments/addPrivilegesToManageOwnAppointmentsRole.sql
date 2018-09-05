f03ee74b78f34cc0ac16be383141ca40d2e40bff
Saikumar <saikv@thoughtworks.com>
2018-09-05 15:51:25 +0530

    Saikumar, Neha | [Add self appointments privilege] | [Feedback] Add ManageOwnAppointments role to Cairo config

---
+++ openmrs/migrations/appointments/addPrivilegesToManageOwnAppointmentsRole.sql
@@ -0,0 +1,8 @@
+INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageOwnAppointments','app:appointments');
+INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageOwnAppointments','Get Locations');
+INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageOwnAppointments','Get Patient Identifiers');
+INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageOwnAppointments','Get Patients');
+INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageOwnAppointments','Get Providers');
+INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageOwnAppointments','View Appointment Services');
+INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageOwnAppointments','View Appointments');
+INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageOwnAppointments','Manage Own Appointments');
