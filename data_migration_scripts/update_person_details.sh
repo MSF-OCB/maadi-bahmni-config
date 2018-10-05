3fff17275f4e0129d8aa430e280d05dcc854f59e
matirp235 <dpritam@thoughtworks.com>
2018-10-05 16:57:07 +0530

    pritam | adding birthestimate and removing area field from script

--- data_migration_scripts/update_person_details.sh
+++ data_migration_scripts/update_person_details.sh
@@ -6,12 +6,12 @@ do
                 family_name = '' where person_id = (select patient_id from patient_identifier where identifier = '${row[0]}'
                 and voided = 0 ); "
 
-                person_details=" update person set gender = '${row[5]}' , birthdate = '${row[6]}' , date_created = '${row[1]}'
+                person_details=" update person set gender = '${row[5]}' , birthdate = '${row[6]}' ,birthdate_estimated = '${row[7]}', date_created = '${row[1]}'
                 where person_id = (select patient_id from patient_identifier where identifier = '${row[0]}' and voided = 0 ); "
-                
+
                 patient_datecreated=" update patient set  date_created = '${row[1]}'  where patient_id = (select patient_id from
                 patient_identifier where identifier = '${row[0]}' and voided = 0 ); "
- 
+
     updateQuery+="$person_name_details $person_details $patient_datecreated"
 
 done < $1
